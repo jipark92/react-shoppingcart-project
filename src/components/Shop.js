c36f41461bae789e3145a8f36df726d842283831
ji <jiyongprk@gmail.com>
2022-04-11 14:58:01 -0400

    lay out items on display

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import clothesDatas from '../clothesData'
 
 export default function Shop() {
+
+
+    console.log(clothesDatas)
+    //show clothes on display
+    const showClothesDisplay = clothesDatas.map((clothesData)=>{
+        return (
+            <div className='clothes-container'key={clothesData.id}>
+                <div className='img-container'>
+                    <img src={clothesData.img} alt="clothes image" className='clothes-img'/>
+                </div>
+                <div className="item-info-container">
+                    <p><b>{clothesData.name}</b></p>
+                    <br></br>
+                    <p>{clothesData.price}</p>
+                </div>
+                <br></br>
+                <div className='btn-container'>
+                    <button className='add-to-cart-btn'>Add To Cart</button>
+                </div> 
+            </div>
+        )
+    })
+
     return (
         <div className="shop-container">
-            <h3>shop</h3>
+            {showClothesDisplay}
         </div>
     )
 }
