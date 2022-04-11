47820b4a07eb2787acef6815d8e20391d17a9230
ji <jiyongprk@gmail.com>
2022-04-11 14:57:41 -0400

    finish home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 export default function Home() {
     return (
@@ -8,8 +10,11 @@ export default function Home() {
                     <h1>More</h1>
                     <h1>Ways</h1>
                     <h1>To</h1>
+                    <h1>Be</h1>
                     <h1>You</h1>
-                    <button className='shopnow-btn'>Shop Now</button>
+                    <Link to = '/shop'>
+                        <button className='shopnow-btn'>Shop Now</button>
+                    </Link>
                 </div>
                 <div className='motto-img-container'>
                     <img src="https://images.unsplash.com/photo-1441986300917-64674bd600d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="motto image" className='motto-image'/>
