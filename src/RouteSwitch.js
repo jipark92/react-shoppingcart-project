6ef303fe94f707061a8e51fab932e157b8c48724
ji <jiyongprk@gmail.com>
2022-04-11 12:37:28 -0400

    add home nav

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from './components/Nav'
+import Home from './components/Home';
+import Shop from './components/Shop';
 
 export default function RouteSwitch() {
     return (
         <BrowserRouter>
+            <Nav/>
             <Routes>
-                <Route path='/nav' element={<Nav />}/>
+                <Route path='/home' element={<Home/>}/> 
+                <Route path='/shop' element={<Shop/>}/>
             </Routes>
         </BrowserRouter>
     )
