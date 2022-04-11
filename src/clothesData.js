d18a09386a0fe9e09dfa8294fe9646d3d42fd230
ji <jiyongprk@gmail.com>
2022-04-11 14:58:31 -0400

    create data file

---
+++ src/clothesData.js
@@ -0,0 +1,64 @@
+export default [{
+    "id": 0,
+    "img": "https://images.urbndata.com/is/image/UrbanOutfitters/61818373_008_d?$medium$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    "name": "New Balance 574 GORE-TEX Sneaker",
+    "price": "$150.00",
+    "quantity": 0
+},
+{
+    "id": 1,
+    "img": "https://images.urbndata.com/is/image/UrbanOutfitters/66414004_004_d?$medium$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    "name": "Katin Local Cord Volley Short",
+    "price": "$65.00",
+    "quantity": 0
+},
+{
+    "id": 2,
+    "img": "https://images.urbndata.com/is/image/UrbanOutfitters/64176423_010_d?$medium$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    "name": "Adidas United Tee",
+    "price": "$35.00",
+    "quantity": 0
+},
+{
+    "id": 3,
+    "img": "https://images.urbndata.com/is/image/UrbanOutfitters/64778665_001_d?$medium$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    "name": "The North Face Lunar New Year Tee",
+    "price": "$35.00",
+    "quantity": 0
+},
+{
+    "id": 4,
+    "img": "https://images.urbndata.com/is/image/UrbanOutfitters/62779269_016_d?$medium$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    "name": "Smiley Bucket Hat",
+    "price": "$30.00",
+    "quantity": 0
+},
+{
+    "id": 5,
+    "img": "https://images.urbndata.com/is/image/UrbanOutfitters/67880997_012_d?$medium$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    "name": "Tupac Me Against The World Tee",
+    "price": "$39.00",
+    "quantity": 0
+},
+{
+    "id": 6,
+    "img": "https://images.urbndata.com/is/image/UrbanOutfitters/66367863_060_d?$medium$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    "name": "Polo Ralph Lauren Paint Splatter Logo",
+    "price": "$65.00",
+    "quantity": 0
+},
+{
+    "id": 7,
+    "img": "https://images.urbndata.com/is/image/UrbanOutfitters/65896250_001_d?$medium$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    "name": "UO Daisy Checkerboard Print Swim Short",
+    "price": "$39.00",
+    "quantity": 0
+},
+{
+    "id": 8,
+    "img": "https://images.urbndata.com/is/image/UrbanOutfitters/67301226_046_d?$medium$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349",
+    "name": "UO Reggie Soccer Jersey Tee",
+    "price": "$55.00",
+    "quantity": 0
+}
+]
