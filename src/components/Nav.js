2a1783458bb616bba370043b28410122aae51784
ji <jiyongprk@gmail.com>
2022-04-11 12:37:57 -0400

    make nav take you to different page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Nav() {
     return (
         <header className='header-container'>
-            <h1>FakeStore</h1>
+            <Link to="/home">
+                <h1 className='store-name'><u>FakeStore</u></h1>
+            </Link>
             <nav className='nav-container'>
                 <ul className='navigation'>
-                    <li>Home</li>
-                    <li>Product</li>
-                    <li>Contact</li>
-                    {/* <li>Cart</li> */}
+                    <Link to ='/home'>
+                        <li>Home</li>
+                    </Link>
+                    <Link to ='/shop'>
+                        <li>Shop</li>
+                    </Link>
                 </ul>
             </nav>
         </header>
