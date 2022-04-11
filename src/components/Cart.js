0b9252e4ff501e3910b45f15169fc3cd2d0b67e3
ji <jiyongprk@gmail.com>
2022-04-11 17:17:52 -0400

    show items in cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,34 @@
 import React from 'react'
 
 export default function Cart(props) {
+
+    const showItem1 = () => {
+        return (
+            <div className='item-1-cart'>
+                <img src={props.quantity1 > 0 ? "https://images.urbndata.com/is/image/UrbanOutfitters/61818373_008_d?$medium$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349" : ""} alt="pic1" className='item1-cart'/>
+                <p>Name: {props.quantity1 > 0 ? "New Balance 574 GORE-TEX Sneaker": ""}</p>
+                <p>Price: {props.quantity1 > 0 ? props.quantity1 * 150: ""}</p>
+                <p>Quantity: {props.quantity1}</p>
+            </div>
+        )
+    }
+
+    const showItem2 = () => {
+        return (
+            <div className='item-2-cart'>
+                <img src={props.quantity2 > 0 ? "https://images.urbndata.com/is/image/UrbanOutfitters/66414004_004_d?$medium$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349" : ""} alt="pic1" className='item2-cart'/>
+                <p>Name: {props.quantity2 > 0 ? "Katin Local Cord": ""}</p>
+                <p>Price: {props.quantity2 > 0 ? props.quantity2 * 65: ""}</p>
+                <p>Quantity: {props.quantity2}</p>
+            </div>
+        )
+    }
+    
     return (
         <div>
-            <h3>Carts:</h3>
-            <p>{props.quantity}</p>
+            <h3>Your Shopping Cart</h3>
+            {props.quantity1 > 0 ? showItem1() : ""}
+            {props.quantity2 > 0 ? showItem2() : ""}
         </div>
     )
 }
