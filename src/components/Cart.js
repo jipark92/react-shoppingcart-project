dc2b0b1545450c46830c7172c4d5d962ac27839d
ji <jiyongprk@gmail.com>
2022-04-11 17:59:05 -0400

    decrease quantity working and show if quantity is more than 1 and remove if less than 1 function

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Cart(props) {
 
@@ -9,17 +9,21 @@ export default function Cart(props) {
                 <p>Name: {props.quantity1 > 0 ? "New Balance 574 GORE-TEX Sneaker": ""}</p>
                 <p>Price: {props.quantity1 > 0 ? props.quantity1 * 150: ""}</p>
                 <p>Quantity: {props.quantity1}</p>
+                <button onClick={props.decreaseClick} id="0">Decrease</button>
+                <button>Increase</button>
             </div>
         )
     }
 
     const showItem2 = () => {
         return (
-            <div className='item-2-cart'>
+            <div className='item-2-cart' >
                 <img src={props.quantity2 > 0 ? "https://images.urbndata.com/is/image/UrbanOutfitters/66414004_004_d?$medium$&fit=constrain&fmt=webp&hei=523&qlt=80&wid=349" : ""} alt="pic1" className='item2-cart'/>
                 <p>Name: {props.quantity2 > 0 ? "Katin Local Cord": ""}</p>
                 <p>Price: {props.quantity2 > 0 ? props.quantity2 * 65: ""}</p>
                 <p>Quantity: {props.quantity2}</p>
+                <button onClick={props.decreaseClick} id="1" >Decrease</button>
+                <button>Increase</button>
             </div>
         )
     }
