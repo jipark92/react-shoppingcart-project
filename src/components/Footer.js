c222aaf55e9e43c48acb1c15348422dc214a2cbe
ji <jiyongprk@gmail.com>
2022-04-12 00:19:22 -0400

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <div className='footer-container'>
+            <footer>Created by <a href="https://github.com/norfkorean/react-shoppingcart-project">Ji Park</a></footer>
+        </div>
+    )
+}
